ae22a0f6c854861488581836b42a0b913e0aa0a8
Yaroslav Boyko <yaroslav.bojko.82@bk.ru>
2024-01-13 17:12:37 +0300

    finish

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    int thisDay;
+    int thisMonth;
+    int thisYear;
+    std::cout << "Enter today's date (day, month, year): ";
+    std::cin >> thisDay >> thisMonth >> thisYear;
+
+    int birthDay;
+    int birthMonth;
+    int birthYear;
+    std::cout << "Enter date of birth (day, month, year): ";
+    std::cin >> birthDay >> birthMonth >> birthYear;
+
+    if ((thisYear > birthYear + 18) || (thisYear == birthYear + 18 && thisMonth > birthMonth) ||
+    (thisYear == birthYear + 18 && thisMonth == birthMonth && thisDay > birthDay))
+        std::cout << "CAN!!!" << std::endl;
+    else
+        std::cout << "It is forbidden!!!" << std::endl;
 }
